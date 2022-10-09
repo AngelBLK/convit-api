297782a849e4c9e2c881cb8fda3b2f776d86dabb
AngelBLK <jaidenze@gmail.com>
2022-10-08 19:33:46 -0500

    Initial configuration started

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+import express, { json } from 'express';
+
+const app = express();
+
+app.use( (req, res, next) => {
+  res.status(404).json({ message: 'endpoint not found'});
+} );
+
+export default app;
