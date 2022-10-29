aeae8de5b5e609e3fcf69268808f03e335a80d44
AngelBLK <jaidenze@gmail.com>
2022-10-29 12:20:33 -0500

    [UPDATE] user schema

--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -1,9 +1,19 @@
 import Joi from "joi";
 
-const email = Joi.string();
-const password = Joi.string();
+const id = Joi.number().integer();
+const email = Joi.string().email;
+const password = Joi.string().min(8);
 
 export const logInUserSchema = Joi.object({
   email: email.required(),
-  password: password.required()
+  password: password.required(),
+});
+
+export const updateUserSchema = Joi.object({
+  email: email,
+  password: password,
+});
+
+export const getUserSchema = Joi.object({
+  id: id.required(),
 });
